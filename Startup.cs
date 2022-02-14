19d7029cdcdc49dde083dd8cb34f316da73b2f77
John Vitor Constant <john.lourenco@vaivoa.com>
2022-02-14 15:32:14 -0300

    removendo o https

--- Startup.cs
+++ Startup.cs
@@ -40,12 +40,12 @@ namespace CrescimentoExponencial
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CrescimentoExponencial v1"));
-            }
+
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CrescimentoExponencial v1"));
+
 
             app.UseHttpsRedirection();
 
